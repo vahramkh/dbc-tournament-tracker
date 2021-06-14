c0c883f0789ee22ae1b2a38864ba69686a227ab2
vahramkh <vahram.khachikyan@gmail.com>
2021-06-14 02:04:29 -0500

    Final COmmit

--- Untitled-1.py
+++ Untitled-1.py
@@ -32,7 +32,28 @@ def menu():
             return userInput 
             break 
 
-#Function to help with the sign up
+#Function to help with viewing the participants
+def viewparticipants():
+    print("View Participants\n====================") 
+    while True:
+        try:
+            startingSlot = int(input("Desired starting slot #[1-" + str(count_of_participants) +"]:"))       
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        if startingSlot > count_of_participants:
+            print("Your input is greater than " + str(count_of_participants)+ ": Please input a smaller integer")
+        elif(int(startingSlot) < 1):
+            print("Type a positive integer")
+        else:
+            for i in range(startingSlot-1,len(participants)):
+                if participants[i] == None:
+                    print(str(i+1) + ": [empty]")
+                else:
+                    print(str(i+1) + ": " + participants[i])
+            break 
+            return None
+
 def signup():
     print("Participant Sign Up\n====================")
     while True:
@@ -67,6 +88,37 @@ def signup():
             return None
 
 
+
+def cancel():
+    print("Participant Cancellation\n================")
+    while True:
+        try:
+            userInput = int(input("Starting slot #[1-" + str(count_of_participants) + "]:"))       
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        if userInput > count_of_participants or userInput < 1:
+            print("Thats not a valid option.")
+        else:
+            userInputName = input("Participant Name: ")
+            if userInputName == participants[userInput-1]:
+                participants[userInput-1] = None
+                print(userInputName + " has been cancelled from starting slot #" + str(userInput))
+            else:
+                print(userInputName + " is not in that starting slot")
+            break
+
+
+def exit():
+    print("Exit\n================")
+    while True:
+        userInput = input("Are you sure you want to exit? Type yes for exit, no to go back to main menu\nAll the data will be lost: ")     
+        if userInput == "yes":
+            return userInput
+        elif userInput == "no":
+            return userInput
+        else:
+            print("That is not a valid input")
      
 
 #MAIN PROGRAM STARTS HERE:
@@ -76,12 +128,18 @@ participants = []
 for i in range(0,count_of_participants):
     participants.append(None)
 print("There are " + str(len(participants)) + " participant slots ready for sign-ups.")
-menu_option = menu()
-if(menu_option == 1):
-    signup()
-#elif(menu_option == 2):
-#
-#elif(menu_option == 3):
 
-#else:
+while True:
+    menu_option = menu()
+    if(menu_option == 1):
+        signup()
+    elif(menu_option == 2):
+        cancel()
+    elif(menu_option == 3):
+        viewparticipants()
+    else:
+        answer = exit()
+        if answer == "yes":
+            print("Goodbye")
+            break
 
